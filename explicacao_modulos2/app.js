0d3d32117c71c2c33a10e06026ab4ad97fb51415
Matheus Gobetti <matheusgobetti12@gmail.com>
2021-08-27 17:59:44 -0300

    Explicações sobre modularizaçao de código no nodejs - parte 2

---
+++ explicacao_modulos2/app.js
@@ -0,0 +1,13 @@
+// const multiplicacao = require('./mod');
+// console.log(multiplicacao(2, 2));
+const Cachorro = require('./Z/mod2');
+
+const cachorrinho = new Cachorro('Border Collie');
+
+cachorrinho.latir();
+
+// const path = require('path');
+// console.log(path.resolve(__dirname, '..', '..'));
+
+console.log(__filename);
+console.log(__dirname);
